d28e0904158b61d94764e948a3e809b58ef0d41d
maxgavaga <ferrarif138@yandex.ru>
2022-06-26 22:28:26 +0300

    Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Загадайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечетных позициях.
+
+Console.Write($"Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine()); 
+
+int RandomNumbers(int size, int min, int max)
+  {
+  int[] randomNumbers = new int[size];
+  int sum = 0;
+  Console.Write("Получившийся массив: ");
+
+    for (int i = 0; i <randomNumbers.Length; i++ )
+    {
+      randomNumbers[i] = new Random().Next(min, max);
+
+      Console.Write(randomNumbers[i] + " ");
+
+      if (i % 2 != 1)
+      {
+        sum = sum + randomNumbers[i];
+      }
+    }
+  return sum;
+  }
+
+int randomNumbers =  RandomNumbers(size, 1, 10);
+
+Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях = {randomNumbers}");
