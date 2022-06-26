aab57b433d9290b12da232f77ac6abb38cf1111f
maxgavaga <ferrarif138@yandex.ru>
2022-06-26 22:12:25 +0300

    Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте массив заполненный случайными положительными трехзначными числами. 
+// Напишите программу, которая покажет количество четных чисел в массиве.
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Получившийся массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+if (numbers[i] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел, {count} числа из массива являются четными");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int x = 0; x < numbers.Length; x++)
+    {
+        numbers[x] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int x = 0; x < numbers.Length; x++)
+    {
+        Console.Write(numbers[x] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
